cb150927d289bcfc06e8236e004eef3f12085a75
sara-dsn <sabr.tdg@gmail.com>
2024-01-05 12:31:00 +0100

    c

--- jarditou.php
+++ jarditou.php
@@ -34,13 +34,27 @@ class employe
             $duree=$aujourdhui->diff($_embauche);
             return $duree->y;
 }
+
+public function prime($_anciennete){
+    $coeff=$_anciennete*2;
+    return $coeff;
+}
+
    public function profil(){
+    $aujourdhui=new DateTime("2024-05-01");
+    $payday=new DateTime("2000-05-01");
     $_datembauche=new DateTime($this->_embauche);
     $anciennete=$this->anciennete($_datembauche);
-        echo "Nom : ".$this->_nom."<br>Prénom : ".$this->_prenom."<br>Date d'embauche : ".$_datembauche->format('Y')." depuis ".$anciennete."<br>Poste : ".$this->_poste."<br>Salaire : ".$this->_salaire."<br>Lieu : ".$this->_lieu;
+    $prime=$this->_salaire/100*(105+$this->prime($anciennete));
+      
+     echo "<br>Nom : ".$this->_nom."<br>Prénom : ".$this->_prenom."<br>Date d'embauche : ".$_datembauche->format('d/m/Y')." depuis ".$anciennete."<br>Poste : ".$this->_poste."<br>Salaire : ".$this->_salaire."<br>Lieu : ".$this->_lieu."<br><br>";
+    
+if($aujourdhui->format("m-d")==$payday->format('m-d')){
+        echo "<br> L'ordre de transfert de ".$prime." a été envoyé à la banque de ".$this->_prenom."<br><br>";
     }
-  
-}
+
+}}
+
 
 
  
@@ -51,5 +65,29 @@ $employe->setEmbauche('2000-01-01');
 $employe->setPoste("Présidente");
 $employe->setSalaire('50000');
 $employe->setLieu("Marseille");
+$moe= new employe();
+$moe->setNom("sizlak");
+$moe-> setPrenom("moe");
+$moe->setEmbauche('1971-10-03');
+$moe->setPoste("barman");
+$moe->setSalaire('2200');
+$moe->setLieu("springfiled");
+$stan= new employe();
+$stan->setNom("smith");
+$stan-> setPrenom("stan");
+$stan->setEmbauche('1972-12-12');
+$stan->setPoste("vigile");
+$stan->setSalaire('2500');
+$stan->setLieu("paris");
+$roger= new employe();
+$roger->setNom("smith");
+$roger-> setPrenom("roger");
+$roger->setEmbauche('1923-08-25');
+$roger->setPoste("vendeur");
+$roger->setSalaire('1800');
+$roger->setLieu("nantes");
 echo $employe->profil();
+echo $stan->profil();
+echo $moe->profil();
+echo $roger->profil();
 ?>
